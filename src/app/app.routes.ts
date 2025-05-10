bc1aae59d59da31d7ecdc538a117f5e6233bc655
awadeLATAM <maurxio2@gmail.com>
2025-05-10 00:45:41 -0500

    feat(in-app-navigation): setup in-app navigation rules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: '',                 redirectTo: 'home', pathMatch: 'full' },
+ { path: '**',               component: PageNotFoundComponent }
+];
+
