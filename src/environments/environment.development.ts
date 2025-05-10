8b7d05efb3d908718661903e046b2a8eadb8e550
awadeLATAM <maurxio2@gmail.com>
2025-05-10 00:04:07 -0500

    feat(environments): add courses endpoint path.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
     production: false,
+    serverBaseUrl: 'http://localhost:3000/api/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
     production: true,
+    serverBaseUrl: 'http://localhost:3000/api/v1',
 };
