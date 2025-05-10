58a2c7bbe7e4712af723f9b375dc5b43652b9763
awadeLATAM <maurxio2@gmail.com>
2025-05-09 22:23:01 -0500

    feat(i18n): configure internationalization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+ selector: 'app-root',
+ standalone: true,
+ imports: [],
+ templateUrl: './app.component.html',
+ styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mecanet-frontend';
+ title = 'mecanet-frontend';
+ constructor(private translate: TranslateService) {
+   this.translate.addLangs(['en', 'es']);
+   this.translate.setDefaultLang('en');
+   this.translate.use('en');
+ }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-import { routes } from './app.routes';
+const httpLoaderFactory: (http: HttpClient) =>
+TranslateLoader = (http: HttpClient) =>
+ new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+ providers: [
+   provideZoneChangeDetection({eventCoalescing: true}),
+   provideHttpClient(),
+   provideTranslateService({
+     loader: {
+       provide: TranslateLoader,
+       useFactory: httpLoaderFactory,
+       deps: [HttpClient]
+     },
+     defaultLanguage: 'en',
+   })],
 };
