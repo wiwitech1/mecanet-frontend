925573b57afa749ab636447fd4a4edee24c7e468
Claus <cbm4y0@gmail.com>
2025-05-13 09:11:12 -0500

    hover notifications

--- src/app/shared/components/notifications-container/notification/notification.component.ts
+++ src/app/shared/components/notifications-container/notification/notification.component.ts
@@ -1,28 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 
-export type NotificationType = 'error' | 'notification';
+export type NotificationType = 'error' | 'notification'; 
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [CommonModule,MatIconModule],
+  imports: [CommonModule, MatIconModule, MatRippleModule],
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent {
   @Input() title: string = '';  // Título de la notificación
   @Input() message: string = ''; // Mensaje de la notificación
-  @Input() type: 'error' | 'notification' = 'notification';  // Tipo de notificación
-  @Input() icon: string = ''; // Icono a mostrar en la notificación
-
-  // Clase dinámica para el icono
-  getIconClass() {
-    return this.type === 'error' ? 'error' : 'notification';
+  @Input() type: NotificationType = 'notification';  // Tipo de notificación
+  @Input() icon: string = 'notification'; // Icono por defecto
+  
+  // Para configurar el ripple
+  rippleColor = 'rgba(0, 0, 0, 0.1)'; // Color sutil para el efecto ripple
+  
+  getNotificationClass(): string {
+    return `notification-${this.type}`;
   }
   
-  getNotificationClass() {
-    return this.type === 'error' ? 'notification-error' : 'notification-info';
+  getIconClass(): string {
+    return `icon-${this.type}`;
   }
 }
