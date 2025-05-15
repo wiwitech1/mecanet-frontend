d870c2ca4b5bc3453b368f6957525135d33644b5
awadeLATAM <maurxio2@gmail.com>
2025-05-15 02:31:12 -0500

    feat: home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductionLineViewComponent } from './features/asset-management/views/production-line-view/production-line-view.component';
 import { PlantViewComponent } from './features/asset-management/views/plant-view/plant-view.component';
+import { HomeComponent } from './public/pages/home/home.component';
 const PageNotFoundComponent = () => import('./public/pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent);
 const ComponentsDemoComponent = (): Promise<any> => import('./shared/views/components-demo/components-demo.component').then(m => m.ComponentsDemoComponent);
 const NewsViewComponent = (): Promise<any> => import('./features/news/views/news-view/news-view.component').then(m => m.NewsViewComponent);
@@ -15,8 +16,7 @@ const PropruebaComponent = (): Promise<any> => import('./features/shared/product
 
 export const routes: Routes = [
     { path: 'components-demo',  loadComponent: ComponentsDemoComponent },
-    { path: 'news',            loadComponent: NewsViewComponent },
-    { path: '',                redirectTo: '/components-demo', pathMatch: 'full' },
+    { path: '',                component: HomeComponent },
     { path: 'activos/plantas',   component: PlantViewComponent },
     { path: 'activos/lineas-produccion',   component: ProductionLineViewComponent },
     { path: 'activos/maquinarias',   loadComponent: MachineryAssetViewComponent },
